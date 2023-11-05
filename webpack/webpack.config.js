8c175ee934b84f92b3f65ac256bec20430c139a4
= <=>
2023-11-05 21:12:39 +0530

    Created basic structure for ERP project

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,1 +1,43 @@
-const path = require('')
+const path = require('path')
+const HTMLWebpackplugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, "..", './public/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader' }
+                ],
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.(png|jpg|jpeg)$/,
+                type:'asset/resource'
+            },
+            {
+                test:/\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type:'asset/inline'
+            },
+        ],
+    },
+    output:{
+        path:path.resolve(__dirname,'..','/build'),
+        filename:'bundle.js'
+    },
+    mode:'development',
+    plugins:[
+        new HTMLWebpackplugin({
+            template:path.resolve(__dirname,'..','./public/index.html')
+        })
+    ]
+
+}
